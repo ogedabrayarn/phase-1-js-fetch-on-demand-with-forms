a3d776572500f0d6b6ce20739aecac2bc639573a
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-25 23:10:49 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    document.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID')
+        // console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector(`section#movieDetails h4`);
+            const summary = document.querySelector(`section#movieDetails p`);
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+            // console.log(data);
+            inputForm.reset();
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
